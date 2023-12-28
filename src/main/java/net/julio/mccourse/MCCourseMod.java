7b046809b91b7d038694d31740e76a904396c937
JoKradept <jokradept@gmail.com>
2023-12-28 00:01:56 -0600

    added a brownblock item

--- src/main/java/net/julio/mccourse/MCCourseMod.java
+++ src/main/java/net/julio/mccourse/MCCourseMod.java
@@ -2,6 +2,7 @@ package net.julio.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.julio.mccourse.block.ModBlocks;
 import net.julio.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,5 +15,6 @@ public class MCCourseMod implements ModInitializer {
 	public void onInitialize() {
 
 		ModItems.registerModUItems();
+		ModBlocks.registerModBlocks();
 	}
 }
--- src/main/java/net/julio/mccourse/item/ModItems.java
+++ src/main/java/net/julio/mccourse/item/ModItems.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.julio.mccourse.MCCourseMod;
+import net.julio.mccourse.block.ModBlocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -22,6 +23,8 @@ public class ModItems {
 
     private static  void itemGroupIngredients (FabricItemGroupEntries entries){
         entries.add(JOJACOLA);
+
+        entries.add(ModBlocks.BROWNBLOCK);
     }
 
     public static void registerModUItems(){
